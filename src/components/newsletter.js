5243c1347f4a37097bd16d921d82e4d094a17887
ActarusGo <76884611+ActarusGo@users.noreply.github.com>
2021-01-03 23:11:27 +0100

    Update newsletter.js

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -5,11 +5,8 @@ const Newsletter = () => {
         <div className="container contact-wrapper text-slate">
           <div className="row text-center mb-4 pt-5">
             <h4 className="col-12 text-slate-dark heading-bigger mb-4">
-              Get The Latest PayBear Updates
+              Get The Latest Updates
             </h4>
-            <p className="col-12 mb-sm-4 mb-md-4">
-              And be the first to know when our crowdsale launches!
-            </p>
           </div>
           <div className="row justify-content-center">
             <form action="" className="col-12 col-lg-7">
@@ -29,11 +26,11 @@ const Newsletter = () => {
           </div>
           <div className="row text-center mt-lg-4 pb-5">
             <p className="col-12 mb-sm-4 mb-md-4">
-              Become part of our ever growing community.
+              Become part of our ever growing community. 
               <a
                 href="https://t.me/ShieldDAO"
                 target="_blank"
-                title="Join our telegram group"
+                title=" Join our telegram group"
               >
                 Join us on Telegram.
               </a>
